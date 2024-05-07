c9d52412b2df146cf6ad7dcbd291cf139a88e8ce
colorado-quinn <zachary.quinn.professional@gmail.com>
2024-05-06 21:14:54 -0700

    5 add data

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.css']
+  styleUrls: ['./housing-list.component.css'],
 })
 export class HousingListComponent implements OnInit {
+  @Input() locationList: HousingLocation[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   searchHousingLocations(searchText: string) {
     console.log(searchText);
   }
- 
 }
